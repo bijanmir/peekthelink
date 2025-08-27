dfd959245493cfb2c78f39461eb7b751900471a8
Bijan Mirfakhrai <bijanxx@gmail.com>
2025-08-27 10:36:44 -0700

    Fix Blade syntax for displaying username in users management view

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -83,7 +83,7 @@
                                 </div>
                                 <div class="ml-4">
                                     <div class="text-sm font-medium text-gray-900">{{ $user->name }}</div>
-                                    <div class="text-sm text-gray-500">@{{ $user->username }}</div>
+                                    <div class="text-sm text-gray-500">@ {{ $user->username }}</div>
                                 </div>
                             </div>
                         </td>
